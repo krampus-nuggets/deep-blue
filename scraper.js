8fd135048a15735f136c3c5c398dd7dc7de7ae92
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-23 22:29:12 +0200

    define "browser"

--- scraper.js
+++ scraper.js
@@ -5,3 +5,10 @@ import puppeteer from "puppeteer";
 
 let searchURL = "<url-here>";
 
+( async() => {
+  // Define headless browser
+  const headless = await puppeteer.launch({ headless: true }); // Set headless browser true | false
+  const pageViewer = await browser.newPage();
+  await pageViewer.setViewport({ width: 1366, height: 768 }); // Define viewing device size
+  await pageViewer.goto(searchURL); // Define URL for puppeteer to digest
+
