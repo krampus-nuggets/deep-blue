0417d6de8fdcfe1a999e9a8b5b4da1b143d7da0b
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-24 00:32:37 +0200

    Add -> Comments

--- scraper.js
+++ scraper.js
@@ -3,21 +3,30 @@
 
 const puppeteer = require("puppeteer");
 
+// Assign URL to let variable
 let searchURL = "<url>";
 
 (async () => {
+  // Chromium Headless - true | false browser window
   const headless = await puppeteer.launch({ headless: true });
+  // Launch scraper on New Page
   const pageLauncher = await headless.newPage();
+  // Set "monitor" size for pageLauncher
   await pageLauncher.setViewport({ width: 1920, height: 1080 });
+  // Launch page from variable holding URL
   await pageLauncher.goto(searchURL);
 
   // Main Data Retriever
   let venueData = await pageLauncher.evaluate(() => {
+    // Array to store values
     let venues = [];
+    // Root block with IDs
     let venueElements = document.querySelectorAll("div.sr_property_block[data-hotelId]");
+    // Main function for processing elements/tags
     venueElements.forEach((venElm) => {
       let venueJSON = {};
       try {
+        // Scrape for Title
         venueJSON.title = venElm.querySelector("span.sr-hotel__name").innerText;
       }
       catch (exception){
@@ -25,7 +34,9 @@ let searchURL = "<url>";
       }
       venues.push(venueJSON);
     });
+    // Print out values/array
     return venues;
   });
+  // Print all object props
   console.dir(venueData);
 })();
